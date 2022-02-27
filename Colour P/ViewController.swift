8895ba5e53bcdd19a978fd24363da4d6a91401bb
PrinceSonnenberg <prince.sonnenberg@gmail.com>
2022-02-27 08:56:16 +0200

    In this update, I added a customer slider and added stack views to stabilise the layout

--- Colour P/ViewController.swift
+++ Colour P/ViewController.swift
@@ -9,26 +9,19 @@ import UIKit
 
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
-    
-    
+
     @IBOutlet weak var colourCollection: UICollectionView!
-    
     @IBOutlet weak var dropperBtn: UIButton!
-    
     @IBOutlet weak var colourSegments: UISegmentedControl!
-    
     @IBOutlet weak var opacityLevel: UISlider!
-    
     @IBOutlet weak var pickedColour: UILabel!
-    
     @IBOutlet weak var opacityPercentage: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
        
-        
+        sliderSettings()
         colourCollection.dataSource = self
-        
         self.view.layoutIfNeeded() //loads before viewdidload
         
     }
@@ -38,6 +31,8 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         print(opacityLevel.value)
         let roundedValue = Int(opacityLevel.value)
         opacityPercentage.text = "\(roundedValue)%"
+        opacityLevel.minimumValue = 0
+        opacityLevel.maximumValue = 100
         
     }
     
@@ -59,7 +54,7 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "colorCell", for: indexPath) //as? CellColor
         
         collectionViewLayoutModification()
-        cell.contentView.backgroundColor = randomColor()
+        cell.contentView.backgroundColor = .blue
         return cell
         
     }
@@ -67,36 +62,53 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
 
-        pickedColour.backgroundColor = randomColor()
+        pickedColour.backgroundColor = .red
         
     }
-    
-    func randomColor() -> UIColor {
-
-        var randomRed:CGFloat = 0.0
-        var randomGreen:CGFloat = 0.0
-        var randomBlue:CGFloat = 0.0
-        let nonRandomAlpha:CGFloat = 1.0
-
-        // reject colors that are too dark -- colors //
-        // are rejected 0.35/3 = %11.6 of the time   //
-        while ( randomRed + randomGreen + randomBlue < 0.30) {
-
-            // for each color channel we generate a random number //
-
-            randomRed = CGFloat(drand48())
-            randomGreen = CGFloat(drand48())
-            randomBlue = CGFloat(drand48())
+    class CustomSlider: UISlider {
+        override func trackRect(forBounds bounds: CGRect) -> CGRect {
+            let point = CGPoint(x: bounds.minX, y: bounds.midY)
+            return CGRect(origin: point, size: CGSize(width: bounds.width, height: 35))
         }
-
-        // build and return a color object   //
-        // with the generated random numbers //
-        return UIColor(red: randomRed,
-          green: randomGreen,
-          blue: randomBlue,
-          alpha: nonRandomAlpha)
-
     }
+    
+    
+    
+    
+    
+    
+    func createColour(){}
+    
+    //creat rgb base vals, insert in loop, return colour=
+    
+    
+    
+//    func randomColor() -> UIColor {
+//
+//        var randomRed:CGFloat = 0.0
+//        var randomGreen:CGFloat = 0.0
+//        var randomBlue:CGFloat = 0.0
+//        let nonRandomAlpha:CGFloat = 1.0
+//
+//        // reject colors that are too dark -- colors //
+//        // are rejected 0.35/3 = %11.6 of the time   //
+//        while ( randomRed + randomGreen + randomBlue < 0.30) {
+//
+//            // for each color channel we generate a random number //
+//
+//            randomRed = CGFloat(drand48())
+//            randomGreen = CGFloat(drand48())
+//            randomBlue = CGFloat(drand48())
+//        }
+//
+//        // build and return a color object   //
+//        // with the generated random numbers //
+//        return UIColor(red: randomRed,
+//          green: randomGreen,
+//          blue: randomBlue,
+//          alpha: nonRandomAlpha)
+//
+//    }
 
     func collectionViewLayoutModification() -> UICollectionViewFlowLayout {
         
@@ -125,5 +137,15 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     }
     
     
+    func sliderSettings(){
+        
+        opacityLevel.setMinimumTrackImage(UIImage(named: "sliderback"),for: .normal)
+        opacityLevel.setMaximumTrackImage(UIImage(named: "sliderback"),for: .normal)
+        opacityLevel.value = 100
+        opacityLevel.isEnabled = true
+    }
 }
+
+
+
 
